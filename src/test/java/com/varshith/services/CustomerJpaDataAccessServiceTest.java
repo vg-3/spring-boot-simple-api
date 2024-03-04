dfa43d2aa42b90961c7200e5f742acf69ba56876
VAGOWDA <varshith.gowda@mercedes-benz.com>
2024-03-04 22:51:22 +0530

    test class for CustomerJpaDataAccessService

--- src/test/java/com/varshith/services/CustomerJpaDataAccessServiceTest.java
+++ src/test/java/com/varshith/services/CustomerJpaDataAccessServiceTest.java
@@ -1,79 +1,82 @@
 package com.varshith.services;
 
+import com.varshith.entities.Customer;
+import com.varshith.repositiories.CustomerRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
 class CustomerJpaDataAccessServiceTest {
 
   private  CustomerJpaDataAccessService underTest;
+  private  AutoCloseable autoCloseable;
+  @Mock
+  private CustomerRepository customerRepository;
 
   @BeforeEach
   void setUp() {
+    autoCloseable = MockitoAnnotations.openMocks(this);
+    underTest = new CustomerJpaDataAccessService(customerRepository);
+  }
 
+  @AfterEach
+  void tearDown() throws Exception {
+    autoCloseable.close();
   }
 
   @Test
   void selectAllCustomers() {
-    //Given
-
-    //When
-
-    //Then
+   underTest.selectAllCustomers();
+    verify(customerRepository).findAll();
   }
 
   @Test
   void selectCustomerById() {
-    //Given
-
-    //When
-
-    //Then
+   Long id  = 1L;
+   underTest.selectCustomerById(id);
+   verify(customerRepository).findById(id);
   }
 
   @Test
   void insertCustomer() {
-    //Given
-
-    //When
-
-    //Then
+    Customer customer =  new Customer(
+        "name","email@gmail.com",20
+    );
+    underTest.insertCustomer(customer);
+    verify(customerRepository).save(customer);
   }
 
   @Test
   void updateCustomer() {
-    //Given
-
-    //When
-
-    //Then
+    Customer customer =  new Customer(
+        "name","email@gmail.com",20
+    );
+    underTest.updateCustomer(customer);
+    verify(customerRepository).save(customer);
   }
 
   @Test
   void deleteCustomer() {
-    //Given
-
-    //When
-
-    //Then
+   Long customerId  = 1L;
+   underTest.deleteCustomer(customerId);
+    verify(customerRepository).deleteById(customerId);
   }
 
   @Test
   void existsPersonWithEmail() {
-    //Given
-
-    //When
-
-    //Then
+   String email= "email@gmail.com";
+   underTest.existsPersonWithEmail(email);
+   verify(customerRepository).existsCustomerByEmail(email);
   }
 
   @Test
   void existsPersonWithId() {
-    //Given
-
-    //When
-
-    //Then
+    Long customerId  = 1L;
+    underTest.existsPersonWithId(customerId);
+    verify(customerRepository).existsCustomerById(customerId);
   }
 }
